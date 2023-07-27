f88e06c6affada82b3777b5b76215c8df2711d62
Seojongki <jongkis@gmail.com>
2023-07-27 13:09:34 +0900

    aa

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <BrowserRouter basename='/thejoeun1-boilerplate1'>
         <App />
     </BrowserRouter>  
 );
